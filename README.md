2588761a8910c68b8105114fced962aed8c42f14
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-02-04 21:53:42 +0800

    preparing

--- README.md
+++ README.md
@@ -17,3 +17,24 @@ I don't know what I am doing.
 The Code world changes very fast. Before you know it, you are obsolete. I will be trying out `twind` instead of `twin.macro` + `emotion`. It purports to be framework-agnostic, & doesn't need a compile step. Which is useful here since I cannot modify the webpack config. If it turns out to be better than what I was using before... welp `interpause-components` gonna need a major version revision...
 
 hey holy crap twind works
+
+## Libraries Used
+
+- Preact: Smaller & Faster React alternative
+  - This comes back to bite me. Cannot alias `preact` to `react` so I can't use its compat layer.
+  - Typically its compat layer means most React libraries can be used, without it...
+  - I am stuck with using libraries that explicitly support Preact or are vanilla
+  - Worth it though
+- Wouter: Minimalist router system
+  - I used this for MetaTTT! My page navigation is built ontop of it
+  - Does its job well despite being super tiny
+- Twind: Tailwind-inspired CSS-in-JS library
+  - Without access to Babel, I couldn't use twin.macro + emotion
+  - This is a decent substitute with quite a few advantages of its own
+  - Lmao but I still think twin.macro + emotion is better
+  - TailwindCSS btw is objectively superior to Bootstrap for niche apps
+- Preact-Feather: the only effing preact icon library I could find that works
+- d3: doesn't fit in the React paradigm but hey I needed to copy chart code
+- Nanostores: Redux & MobX suck
+  - only global state management system i could find with explicit Preact support
+  - looks good though, Im not complaining too much
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import powerbi from 'powerbi-visuals-api'
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions
 
+import { Link, Route } from 'wouter-preact'
+import * as Icon from 'preact-feather'
 import { useEffect, useErrorBoundary, useRef, useState } from 'preact/hooks'
 
 import Map from './Map'
@@ -8,20 +10,6 @@ import { VisualSettings } from './settings'
 
 function App(opt: VisualUpdateOptions) {
   //console.log(opt)
-  /*
-  several doc examples check for whether dataViews exist to do
-  stuff like conditionally show a landing page but looking at
-  the data structure... they seem broken
-  the typing is mostly accurate at least, but there are some
-  inconsistency (usually just missing props which is fine)
-  the greatest hinder is that their naming convention is weird
-  what exactly is a dataView? its not in the docs for sure
-  why are settings called dataViewObjects? am I suppose to mix them up?
-  its almost as if they forced a crusty C# developer to use typescript
-  their documentation is too example-driven, handholds you like a baby
-  but doesn't actually explain anything... no API reference either
-  internal code isn't exposed by the interface pkg so I've to guess a lot
-  */
 
   const dataView = opt.dataViews[0]!
   const settings = VisualSettings.parse<VisualSettings>(dataView)
@@ -55,6 +43,7 @@ function App(opt: VisualUpdateOptions) {
         Insert SVG Map
         <input ref={fileRef} type='file' onInput={onChange}></input>
       </label>
+      <Icon.Settings />
       <p>{fileDidUpload ? 'yay upload' : 'wtf'}</p>
       <p>{imgUrl}</p>
       <div tw='h-full w-full p-0'>
