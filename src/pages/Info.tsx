fce7e9118adc4796875991922286d548e5520f4a
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-02-05 12:09:48 +0800

    include usage info

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -3,6 +3,10 @@ export default function Info() {
     <div>
       <h2 tw='text-2xl'>Info</h2>
       <p>Made by J-H</p>
+      <p>Left-click drag to pan</p>
+      <p>Mouse wheel to zoom</p>
+      <p>Left-click anywhere to reset view</p>
+      <p>Hover on plot circles to see their description</p>
     </div>
   )
 }
